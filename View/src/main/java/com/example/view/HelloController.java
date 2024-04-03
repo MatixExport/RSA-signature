c0f3b62f9ff287b473940be0ad407ebc3c5d57c6
Jakub Kubiś <jakubkubis45@gmail.com>
2024-04-03 17:58:23 +0200

    WIP gui

--- View/src/main/java/com/example/view/HelloController.java
+++ View/src/main/java/com/example/view/HelloController.java
@@ -1,11 +1,30 @@
 package com.example.view;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private Pane main_pane;
+
+
+    public void switchPane (ActionEvent event) throws IOException {
+        String fxml_name  = (String) ((Button)event.getSource()).getUserData();
+        Pane newLoadedPane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(fxml_name)));
+        main_pane.getChildren().removeAll();
+        main_pane.getChildren().add(newLoadedPane);
+    }
+
 
     @FXML
     protected void onHelloButtonClick() {
